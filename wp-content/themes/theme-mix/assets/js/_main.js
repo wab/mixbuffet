6fa591b551aefda12fbee731884670bf58f06374
wab [odev] <wab2com@gmail.com>
2014-08-11 18:36:15 +0200

    less config

--- wp-content/themes/theme-mix/assets/js/_main.js
+++ wp-content/themes/theme-mix/assets/js/_main.js
@@ -24,28 +24,34 @@ var Roots = {
     init: function() {
       // JavaScript to be fired on all pages
 
-      //Scroll to top
-      // hide #back-top first
-      $("#back-top").hide();
-      
-      // fade in #back-top
-      $(function () {
-        $(window).scroll(function () {
-          if ($(this).scrollTop() > 100) {
-            $('#back-top').fadeIn();
-          } else {
-            $('#back-top').fadeOut();
-          }
-        });
+          //Scroll to top
+          // hide #back-top first
+          $("#back-top").hide();
+          
+          // fade in #back-top
+          $(function () {
+            $(window).scroll(function () {
+              if ($(this).scrollTop() > 100) {
+                $('#back-top').fadeIn();
+              } else {
+                $('#back-top').fadeOut();
+              }
+            });
 
-        // scroll body to 0px on click
-        $('#back-top a').click(function () {
-          $('body,html').animate({
-            scrollTop: 0
-          }, 800);
-          return false;
-        });
-      });
+            // scroll body to 0px on click
+            $('#back-top a').click(function () {
+              $('body,html').animate({
+                scrollTop: 0
+              }, 800);
+              return false;
+            });
+          });
+
+          // yamm 
+          $(document).on('click', '.yamm .dropdown-menu', function(e) {
+            e.stopPropagation();
+          });
+
         }
   },
   // Home page
