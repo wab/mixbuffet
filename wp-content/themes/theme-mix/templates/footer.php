4f74a7bae0530bf51db5034c8ed37ffcef836ed6
wab [odev] <wab2com@gmail.com>
2014-08-12 13:12:50 +0200

    footer install

--- wp-content/themes/theme-mix/templates/footer.php
+++ wp-content/themes/theme-mix/templates/footer.php
@@ -1,6 +1,33 @@
 <footer class="content-info" role="contentinfo">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+  <div class="wrap container">
+   	<div class="row">
+   		<div class="col-lg-8">
+   			<div class="wrapper">
+   				<h2>Mix par buffet</h2>
+   				<p>Aliquam nisl enim, tristique tempus placerat at, posuere in lectus. Nunc iaculis risus vel orci ornare dignissim sed vitae nulla. Sed dapibus, lectus sit amet adipiscing egestas, mauris est viverra nibh (iaculis pretium sem orci aliquet mauris). Nulla lobortis tempus commodo. Suspendisse potenti cras molestie, risus a enim convallis vitae luctus libero lacinia. Aliquam tincidunt velit sit amet ante hendrerit tempus.</p>
+   				<p><a href="#" class="btn btn-default">visiter le site</a></p>
+   			</div>
+   			<div class="wrapper">
+   				<h2>Recrutement</h2>
+   				<p><a href="#" class="btn btn-default">Contact recrutement</a></p>
+   			</div>
+   		</div>
+   		<div class="col-lg-4">
+   			<div class="wrapper">
+   				<h2>Coordonnées</h2>
+   				<address>
+   					<strong>za du val coric<br>
+   						BP 101T</strong>
+   					56382 Guer cedex<br>
+   					<a href="mailto:contact@mix-buffet.com">contact@mix-buffet.com</a>
+   				</address>
+   				<hr>
+   				<h2>Contact</h2>
+   				<h3>Vous étes consommateur ou fournisseur ?</h3>
+   				<p><a href="#" class="btn btn-default">Accéder à notre formulaire</a></p>
+   			</div>
+   		</div>
+   	</div>
   </div>
 </footer>
 
