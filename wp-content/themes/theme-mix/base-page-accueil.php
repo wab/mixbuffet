d0a6b253a5843080fdb0af2ed15fcff838a38772
wab [odev] <wab2com@gmail.com>
2014-08-12 12:10:43 +0200

    install gabarit page accueil

---
+++ wp-content/themes/theme-mix/base-page-accueil.php
@@ -0,0 +1,38 @@
+<?php get_template_part('templates/head'); ?>
+<body <?php body_class(); ?>>
+
+  <!--[if lt IE 8]>
+    <div class="alert alert-warning">
+      <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
+    </div>
+  <![endif]-->
+
+  <?php
+    do_action('get_header');
+    get_template_part('templates/header');
+  ?>
+
+  <section class="section">
+    <div class="container">
+      <h1>Slider</h1>
+    </div>
+  </section>
+
+  <div class="wrap container" role="document">
+    <div class="content row">
+      <main class="main <?php echo roots_main_class(); ?>" role="main">
+        <?php include roots_template_path(); ?>
+      </main><!-- /.main -->
+    </div><!-- /.content -->
+  </div><!-- /.wrap -->
+
+   <section class="section">
+    <div class="container">
+      <h1>actus</h1>
+    </div>
+  </section>
+
+  <?php get_template_part('templates/footer'); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/theme-mix/page-accueil.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Template Name: Accueil
+*/
+?>
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/page', 'header'); ?>
+  <?php get_template_part('templates/content', 'page'); ?>
+<?php endwhile; ?>
